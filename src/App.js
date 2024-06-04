faea2a5f34a333775350bdb2cc759c038fb0ba53
pmason <pmason@argenx.com>
2024-06-04 14:17:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import "./App.css";
 
 function App() {
+  const myForm = document.getElementById("myForm");
+  const csvFile = document.getElementById("csvFile");
+
+  myForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const input = csvFile.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const text = e.target.result;
+      Papa.parse(text, {
+        complete: function (results) {
+          console.log(results);
+        },
+        header: true,
+      });
+      // const data = d3.csvParse(text);
+      // document.write(JSON.stringify(data));
+    };
+
+    reader.readAsText(input);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form id="myForm">
+        <input type="file" id="csvFile" accept=".csv" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
   );
 }
-
 export default App;
